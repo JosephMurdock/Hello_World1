5210a7fd716222bb292aa1af18eb9b34f107e04c
jmurdock <jmurdock@209.42.106.199>
2017-01-19 19:48:54 -0500

    This is the best program ever.

---
+++ Hello_World1/src/Hello_World1Package/Main.java
@@ -0,0 +1,14 @@
+package Hello_World1Package;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Shitlords.");
+	}
+
+}
